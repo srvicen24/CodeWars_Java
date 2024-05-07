1d711953223848ad8b5b50d20d2ffbd33626787b
Vicente Marín Lorencio <01vicente01@gmail.com>
2024-05-07 11:39:03 +0200

    Update print statement in Main.java

--- src/app/Main.java
+++ src/app/Main.java
@@ -3,6 +3,6 @@ package src.app;
 public class Main {
     // To run some of the exercises.
     public static void main(String[] args) {
-        System.out.println("Hello, GitHub Copilot!");
+        System.out.println("Hello, World");
     }
 }
